6bdb38ed78335deaec78d5eec4a352c15968af93
Bill <xuyatao415@gmail.com>
2022-09-02 00:46:56 +0800

    chore: update format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule} from "@angular/material/slider";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from "@angular/material/slider";
 
 @NgModule({
   declarations: [
@@ -18,4 +17,5 @@ import { MatSliderModule} from "@angular/material/slider";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
