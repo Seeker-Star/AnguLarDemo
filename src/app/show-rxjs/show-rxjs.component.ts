989a9d3d2045fb59de16975a638056dc4030e4ee
Bill <xuyatao415@gmail.com>
2022-12-25 00:36:53 +0800

    feat: test rxjs

--- src/app/show-rxjs/show-rxjs.component.ts
+++ src/app/show-rxjs/show-rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {interval, take} from "rxjs";
 
 @Component({
   selector: 'app-show-rxjs',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowRxjsComponent implements OnInit {
 
+  showNumber: number = 0;
   constructor() { }
 
   ngOnInit(): void {
+
+    const numbers = interval(10)
+    const takeFourNumbers = numbers.pipe(take(4000));
+    takeFourNumbers.subscribe(x => this.showNumber = x);
   }
 
 }
